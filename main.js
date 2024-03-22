23b2b071ad2bb966bd91e23be68d2f8de1867fbe
shukriAbdul <shukriabdullahi756@gmail.com>
2024-03-21 20:59:39 -0400

    my calculator

---
+++ main.js
@@ -0,0 +1,21 @@
+const display = document.querySelector("#display")
+
+//functions
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value ="";
+
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value )
+    }
+    catch(error){
+        display.value = "Error"
+    }
+}
